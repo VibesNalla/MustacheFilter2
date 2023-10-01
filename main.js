7f8a826a12f2d3ad2d76733c0d13504a56a10320
VibesNalla <117597116+VibesNalla@users.noreply.github.com>
2023-10-01 18:23:13 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+noseX = 0;
+noseY = 0;
+
+function preload(){
+m_image = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
+}
+
+function setup(){
+    canvas = createCanvas(400, 400);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.size(400,400);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log("Posenet is Initialized!!");
+}
+
+function draw(){
+image(video, 0, 0, 400, 400);
+image(m_image, noseX, noseY, 30,30);
+}
+
+function take_snapshot(){
+    save('myfilterimage.png');
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x-15;
+        noseY = results[0].pose.nose.y-10;
+        console.log("noseX = "+ noseX);
+        console.log("noseY = "+ noseY);
+    }
+}
